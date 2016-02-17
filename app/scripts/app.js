4f2b0df08a546e174e02d7b930b5d2839ddf6e82
Andrew Hilts <andrewchilts@gmail.com>
2016-02-17 14:53:03 -0500

    inline dependencies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,7 +26,7 @@ var AMIApp = angular.module('AMIApp', [
     'ui.bootstrap',
     'pascalprecht.translate'
   ])
-  .service('cmsStatus', function($location, NavCollection){
+  .service('cmsStatus', ['$location', 'NavCollection', function($location, NavCollection){
     var online = false;
     var firstRun = true;
     var path;
@@ -63,13 +63,13 @@ var AMIApp = angular.module('AMIApp', [
       }
       firstRun = false;
     }
-  })
-  .service('urls', function(apiDomain, apiRoot, apiPath, enrollmentDomain, enrollmentApiPath, languageCode){
-    this.apiURL = apiDomain + apiRoot + "/" + languageCode + apiPath
-    this.enrollmentURL = enrollmentDomain + enrollmentApiPath
+  }])
+  .service('urls', ['apiDomain', 'apiRoot', 'apiPath', 'enrollmentDomain', 'enrollmentApiPath', 'languageCode', function(apiDomain, apiRoot, apiPath, enrollmentDomain, enrollmentApiPath, languageCode){
+    this.apiURL = apiDomain + apiRoot + "/" + languageCode + apiPath;
+    this.enrollmentURL = enrollmentDomain + enrollmentApiPath;
 
     return this;
-  })
+  }])
   .config(['$translateProvider', function($translateProvider) {
     $translateProvider.useStaticFilesLoader({
       prefix: 'translations/locale-',
@@ -85,7 +85,7 @@ var AMIApp = angular.module('AMIApp', [
           industries: ["dataProviderService", "urls", "AMIRequest", "jurisdictionID", function(dataProviderService, urls, AMIRequest, jurisdictionID) {
             return dataProviderService.getItem(urls.apiURL, "/jurisdictions/" + jurisdictionID + "/industries");
           }]
-        },
+        }
       })
       .when('/operator', {
         templateUrl: 'views/company.html',
@@ -199,7 +199,7 @@ AMIApp.filter('object2Array', function() {
   }
 });
 
-AMIApp.directive('focusMe', function($timeout, $parse) {
+AMIApp.directive('focusMe', ['$timeout', '$parse', function($timeout, $parse) {
   return {
     //scope: true,   // optionally create a child scope
     link: function(scope, element, attrs) {
@@ -215,107 +215,107 @@ AMIApp.directive('focusMe', function($timeout, $parse) {
       });
     }
   };
-});
-// AMIApp.run(function($http, NavCollection, $timeout, $location, $translate, languageCode){
-//   $location.path('/');
-//   $translate.use(languageCode)
-//   .then(function(){
-//     $translate(["nav.start", "nav.org", "nav.ask", "nav.you", "nav.request", "nav.finish"])
-//     .then(function(translations){
-//       var stages = [
-//         {
-//           name: translations['nav.start'],
-//           path: "#/",
-//           id: "start",
-//           icon: "fa fa-home",
-//           restricted: false,
-//           className: "",
-//           target: "_self"
-//         },
-//         {
-//           name: translations['nav.org'],
-//           path: "#/operator",
-//           id: "operator",
-//           icon: "fa fa-briefcase",
-//           restricted: true,
-//           className: "",
-//           target: "_self"
-//         },
-//         {
-//           name: translations['nav.ask'],
-//           path: "#/components",
-//           id: "components",
-//           icon: "fa fa-question-circle",
-//           restricted: true,
-//           className: "",
-//           target: "_self"
-//         },
-//         {
-//           name: translations['nav.you'],
-//           path: "#/subject",
-//           id: "subject",
-//           icon: "fa fa-user",
-//           restricted: true,
-//           className: "",
-//           target: "_self"
-//         },
-//         {
-//           name: translations['nav.request'],
-//           path: "#/request",
-//           id: "request",
-//           icon: "fa fa-file-text",
-//           restricted: true,
-//           className: "",
-//           target: "_self"
-//         },
-//         {
-//           name: translations['nav.finish'],
-//           path: "#/finish",
-//           id: "finish",
-//           icon: "fa fa-flag-checkered",
-//           restricted: true,
-//           target: "_self"
-//         }
-//       ]
-//       angular.forEach(stages, function(item){
-//         NavCollection.addNavItem(item.id, item.path, item.name, item.icon, item.restricted, item.className, item.target);
-//       });
-//     });
-//   });
-// });
-// AMIApp.run(function($templateCache, $http) {
-//   $http.get('views/messages.html')
-//   .then(function(response) {
-//     $templateCache.put('status-messages', response.data); 
-//   });
-// });
-// AMIApp.run(function(urls, jurisdictionID, AMIRequest, cmsStatus, dataProviderService, $interval, $timeout) {
-//    dataProviderService.getItem(urls.apiURL, "/jurisdictions/" + jurisdictionID)
-//     .then(function(jurisdiction){
-//       AMIRequest.set('jurisdiction', jurisdiction);
-//       AMIRequest.markAsComplete('jurisdiction');
-//     }).
-//     catch(function(err){
-//       console.log(err);
-//     })
-//   $interval(function(){
-//     if(!cmsStatus.isOnline()){
-//       var randomInt = Math.floor(Math.random() * (100000000 - 0)) + 0;
-//       dataProviderService.request(urls.apiURL, "/jurisdictions/" + jurisdictionID, {"flag": randomInt}, 'GET', null, false)
-//         .success( function(data, status, headers, config) {
-//           cmsStatus.isOnline(true);
-//           AMIRequest.set('jurisdiction', data);
-//         })
-//         .error( function(data, status, headers, config) {
-//           cmsStatus.isOnline(false);
-//         });
-//     }
-//   }, 60000);
-//   $timeout(function(){
-//       document.getElementById("loadingScreen").className += ' faded-out';
-//       $timeout(function(){
-//         document.getElementById("loadingScreen").className.replace('faded-out', '');
-//         document.getElementById("loadingScreen").remove();
-//       }, 200);
-//     }, 170);
-// });
+}]);
+AMIApp.run(['$http', 'NavCollection', '$timeout', '$location', '$translate', 'languageCode', function($http, NavCollection, $timeout, $location, $translate, languageCode){
+  $location.path('/');
+  $translate.use(languageCode)
+  .then(function(){
+    $translate(["nav.start", "nav.org", "nav.ask", "nav.you", "nav.request", "nav.finish"])
+    .then(function(translations){
+      var stages = [
+        {
+          name: translations['nav.start'],
+          path: "#/",
+          id: "start",
+          icon: "fa fa-home",
+          restricted: false,
+          className: "",
+          target: "_self"
+        },
+        {
+          name: translations['nav.org'],
+          path: "#/operator",
+          id: "operator",
+          icon: "fa fa-briefcase",
+          restricted: true,
+          className: "",
+          target: "_self"
+        },
+        {
+          name: translations['nav.ask'],
+          path: "#/components",
+          id: "components",
+          icon: "fa fa-question-circle",
+          restricted: true,
+          className: "",
+          target: "_self"
+        },
+        {
+          name: translations['nav.you'],
+          path: "#/subject",
+          id: "subject",
+          icon: "fa fa-user",
+          restricted: true,
+          className: "",
+          target: "_self"
+        },
+        {
+          name: translations['nav.request'],
+          path: "#/request",
+          id: "request",
+          icon: "fa fa-file-text",
+          restricted: true,
+          className: "",
+          target: "_self"
+        },
+        {
+          name: translations['nav.finish'],
+          path: "#/finish",
+          id: "finish",
+          icon: "fa fa-flag-checkered",
+          restricted: true,
+          target: "_self"
+        }
+      ];
+      angular.forEach(stages, function(item){
+        NavCollection.addNavItem(item.id, item.path, item.name, item.icon, item.restricted, item.className, item.target);
+      });
+    });
+  });
+}]);
+AMIApp.run(['$templateCache', '$http', function($templateCache, $http) {
+  $http.get('views/messages.html')
+  .then(function(response) {
+    $templateCache.put('status-messages', response.data);
+  });
+}]);
+AMIApp.run(['urls', 'jurisdictionID', 'AMIRequest', 'cmsStatus', 'dataProviderService', '$interval', '$timeout', function(urls, jurisdictionID, AMIRequest, cmsStatus, dataProviderService, $interval, $timeout) {
+   dataProviderService.getItem(urls.apiURL, "/jurisdictions/" + jurisdictionID)
+    .then(function(jurisdiction){
+      AMIRequest.set('jurisdiction', jurisdiction);
+      AMIRequest.markAsComplete('jurisdiction');
+    }).
+    catch(function(err){
+      console.log(err);
+    })
+  $interval(function(){
+    if(!cmsStatus.isOnline()){
+      var randomInt = Math.floor(Math.random() * (100000000 - 0)) + 0;
+      dataProviderService.request(urls.apiURL, "/jurisdictions/" + jurisdictionID, {"flag": randomInt}, 'GET', null, false)
+        .success( function(data, status, headers, config) {
+          cmsStatus.isOnline(true);
+          AMIRequest.set('jurisdiction', data);
+        })
+        .error( function(data, status, headers, config) {
+          cmsStatus.isOnline(false);
+        });
+    }
+  }, 60000);
+  $timeout(function(){
+      document.getElementById("loadingScreen").className += ' faded-out';
+      $timeout(function(){
+        document.getElementById("loadingScreen").className.replace('faded-out', '');
+        document.getElementById("loadingScreen").remove();
+      }, 200);
+    }, 170);
+}]);
