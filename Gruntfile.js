46446cfd009f1455c24c7d54e3378e8110025945
Andrew Hilts <andrewchilts@gmail.com>
2016-03-04 11:25:40 -0500

    change gruntfile ip

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '128.100.127.49',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
