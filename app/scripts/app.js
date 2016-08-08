fbf80c458e18afa92b7092c2bab458bf50606f26
Andrew Hilts <andrewchilts@gmail.com>
2016-08-08 11:36:13 -0400

    locale moment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -435,6 +435,7 @@ AMIApp.run(['$http', 'NavCollection', '$timeout', '$location', '$translate', 'en
   }
   $translate.use(languageCode);
   urls.setLanguageCode(languageCode);
+  moment.lang(languageCode);
   console.log(languageCode);
 
       var stages = [
